b33844c8c4eb3b59395b2c1ae55e0a73b131d3d3
Arthur <arthur.rodriguesgv@gmail.com>
2025-11-17 23:47:29 -0300

    adicionado o remover e melhorado a interface

--- script.js
+++ script.js
@@ -23,11 +23,22 @@ function inserir(event){
 
     i++;
 
-    let linha = '<tr>' +
+    let linha = '<tr id="linha_' + i + '">' +
         '<td id="c'+i+'1">' + nome.value + '</td>' + 
         '<td id="c'+i+'2">' + tel.value + '</td>' +   
         '<td id="c'+i+'3">' + email.value + '</td>' + 
-        '<td id="c'+i+'4"><input type="button" id="bteditar" name="bteditar" value="Editar" onclick="editar(\''+nome.value+'\', '+i+')"></td>' +
+        '<td id="c'+i+'4" class="botoes-acao">' +
+            '<button type="button" class="btn-editar" onclick="editar(' + i + ')">' +
+                '<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">' +
+                '<path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path>' +
+                '</svg>' +
+            '</button>' +
+            '<button type="button" class="btn-apagar" onclick="apagar(' + i + ')">' +
+                '<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">' +
+                '<polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>' +
+                '</svg>' +
+            '</button>' +
+        '</td>' +
         '</tr>';
 
     tabela.innerHTML = tabela.innerHTML + linha;
@@ -37,5 +48,21 @@ function inserir(event){
     email.value = "";
 
     gerenciarModal('fechar');
+}
+
+function apagar(indice){
+
+    let confirmacao = confirm("Tem certeza que deseja apagar?");
+    
+    if(confirmacao) {
+        let apagarLinha = document.getElementById('linha_' + indice);
+        apagarLinha.remove();
+    } else {
+        console.log("Operação cancelada");
+    }
+}
+
+function editar(indice) {
+
     
 }
